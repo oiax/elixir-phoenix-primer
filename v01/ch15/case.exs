c9f58d2e95e50374b331a089b03d8a6d1d8f97d0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-12 23:01:13 +0900

    Add examples for v01/ch15

---
+++ v01/ch15/case.exs
@@ -0,0 +1,6 @@
+n = 3
+case n do
+  1 -> IO.puts "A"
+  2 -> IO.puts "B"
+  _ -> IO.puts "C"
+end
---
+++ v01/ch15/case.rb
@@ -0,0 +1,6 @@
+n = 1
+case n
+  when 1 then puts "A"
+  when 2 then puts "B"
+  else puts "C"
+end
