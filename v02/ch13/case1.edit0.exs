789c5a40049944e842b898ad339e068e3306cd73
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-02-01 02:36:26 +0900

    Update example for v02/ch13

--- v02/ch13/case1.edit0.exs
+++ v02/ch13/case1.edit0.exs
@@ -1,10 +1,12 @@
-t = {:a, 3}
+for x <- [:a, :b, :c, :d] do
+  t = {x, 3}
 
-x = case t do
-  {:a, n} -> n
-  {:b, n} -> n * 2
-  {:c, _} -> 0
-  _ -> -1
+  y = case t do
+    {:a, n} -> n
+    {:b, n} -> n * 2
+    {:c, _} -> 0
+    _ -> -1
+  end
+
+  IO.puts y
 end
-
-IO.puts x
