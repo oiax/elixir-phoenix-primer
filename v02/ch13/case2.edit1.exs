ad04f098f4b77fb7aeaa0331b39148983a5caa4f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-02-01 10:46:17 +0900

    Add an example for v02/ch13

---
+++ v02/ch13/case2.edit1.exs
@@ -0,0 +1,12 @@
+user = %User{name: "Alice"}
+list = [100, [1, 2, 3], %{value: 7}, user, true]
+for e <- list do
+  x = case e do
+    n when is_integer(n) -> n
+    l when is_list(l) -> Enum.join(l, ":")
+    %{__struct__: User} = u -> "name = #{u.name}"
+    m when is_map(m) -> "value = #{m.value}"
+    b when is_boolean(b) -> b
+  end
+  IO.puts x
+end
