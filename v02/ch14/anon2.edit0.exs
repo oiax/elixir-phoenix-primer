c8b89a29f108cc335b8e1341b7a1e90ec12adc66
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-02-05 11:48:56 +0900

    mv v02/ch13/* v02/ch14/

--- v02/ch14/anon2.edit0.exs
+++ v02/ch14/anon2.edit0.exs
@@ -1,3 +1,8 @@
-double2 = fn({atom, n}) -> {atom, n * 2} end
-x = double2.({:a, 7})
+double_or_triple = fn
+  {:a, n} -> {:a, n * 2}
+  {:b, n} -> {:b, n * 3}
+end
+x = double_or_triple.({:a, 7})
+y = double_or_triple.({:b, 5})
 IO.inspect x
+IO.inspect y
